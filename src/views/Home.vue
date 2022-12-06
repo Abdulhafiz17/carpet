30bf2e229d32e3b48d5c80efaf4116d74b1b7678
Abdo <abdo.maxmud@gmail.com>
2022-12-06 09:33:03 +0500

    add seller role to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,29 @@
 <template></template>
 
 <script>
+import swal from "sweetalert";
 export default {
   name: "Home",
   emits: ["setloading"],
   data() {
     return {
-      role: localStorage.getItem("role")
-    }
+      role: localStorage.getItem("role"),
+    };
   },
   created() {
     if (this.role == "admin") {
-      this.$router.push("/filiallar")
+      this.$router.push("/filiallar");
     } else if (this.role == "branch_admin") {
-      this.$router.push("/kassa")
+      this.$router.push("/kassa");
+    } else {
+      swal({
+        icon: "info",
+        text: "Ushbu rolda tizimga kirish mumkin emas",
+        closeOnEsc: false,
+        closeOnClickOutside: false,
+      }).then(() => {
+        this.$router.push("/");
+      });
     }
   },
 };
